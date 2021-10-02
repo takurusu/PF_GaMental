d40196d5bcba87704bbdabd768129aa961c1c61d
takurusu <taku.a.k.a.mizutani@gmail.com>
2021-10-02 12:01:09 +0000

    Create controllers and games table

---
+++ db/migrate/20211002115714_create_games.rb
@@ -0,0 +1,16 @@
+class CreateGames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :games do |t|
+
+      t.integer :user_id, null: false
+      t.integer :image_id, null: false
+      t.string :title, null: false
+      t.string :text, null: false
+      t.datetime :created_at, null: false, default: -> { 'NOW()' }
+      t.datetime :updated_at, null: false, default: -> { 'NOW()' }
+
+      t.timestamps
+
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_112504) do
+ActiveRecord::Schema.define(version: 2021_10_02_115714) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2021_10_02_112504) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "image_id", null: false
+    t.string "title", null: false
+    t.string "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
