0e051f933cff5490447ad1ffaade3703b5f46e74
takurusu <taku.a.k.a.mizutani@gmail.com>
2021-10-05 13:42:12 +0000

    Create admin game [index, show, edit] page

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,2 +1,33 @@
 class Admin::GamesController < ApplicationController
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    redirect_to admin_game_path
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to admin_games_path
+  end
+
+  private
+
+    def game_params
+      params.require(:game).permit(:image, :title, :text)
+    end
+
 end
